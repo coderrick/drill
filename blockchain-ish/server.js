399b21d2dac08757fca59ac5bb50994e6a87c5da
Communications <communications@DMacBook.local>
2018-05-19 17:16:14 -0400

    Refractored server.js, found better way to get ip

--- blockchain-ish/server.js
+++ blockchain-ish/server.js
@@ -1,10 +1,16 @@
+'use strict';
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var ip = require("ip");
+var os = require('os');
+  
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(8080, () => {
+    console.log('App listening on port 8080');
+    console.log(ip.address());
+});
