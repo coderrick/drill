2a3d82268215a8728175fa18446d471cb56dc18a
Communications <communications@DMacBook.local>
2018-05-20 01:43:24 -0400

    Added bash script to install dependencies; Added statements to .sql

---
+++ blockchain-ish/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# declare STRING variable
+npm install -g ethereumjs-testrpc;
+npm install -g truffle;
+echo "DRILL Dapp Dependencies intalled!!"
---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE drill;
+
+CONNECT drill;
+
+CREATE TABLE drill_users (
+  n_id serial PRIMARY KEY,
+  s_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  e_id INTEGER,
+  usr_name VARCHAR(50),
+  location point,
+  heart_rate VARCHAR(50), 
+  metab_rate VARCHAR(90),
+  calories VARCHAR(20),
+  height VARCHAR(20),
+  weight VARCHAR(20),
+  friends VARCHAR(100),
+  leader BOOLEAN NOT NULL
+);
+
+INSERT INTO drill_users (e_id, usr_name, location, heart_rate, metab_rate, calories, height, weight, friends, leader)
+VALUES (923824832042032932, "h_tg", (12.4,11.2), '80/120', '45s', "3000", "60", "200lbs", "j_doe", TRUE);
