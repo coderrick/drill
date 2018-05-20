9dbd3dd6dc0aa737c59e34ad33322a9da2134efa
David Chen <youdinghuan.chen.gr@dartmouth.edu>
2018-05-20 08:18:55 -0400

    Front end latest update

--- activityReport-export/app/index.js
+++ activityReport-export/app/index.js
@@ -1,7 +1,27 @@
-/*
- * Entry point for the watch app
- */
+/* Entry point for the watch app */
 console.log("App Started!");
-
 import document from "document";
-var hours1 = document.getElementById("progressBar");
+
+/* Retrieve heart rate */
+import { HeartRateSensor } from "heart-rate";
+var hrm = new HeartRateSensor();
+
+/* Retrieve metabolic rate*/
+
+/* Retreve meal*/
+
+// Customize progress bar:
+var prog = 0.3; //Use GET request
+var percentage = document.getElementById("progress_dynamic");
+percentage.width = prog * 250;
+
+// Customize progress percentage label:
+// var percLab = document.getElementById("lab_dynamic");
+// console.log(percLab.value);
+// percLab.value = 100*prog + "%";
+// console.log(percLab.value);
+// document.getElementById("lab_dynamic").innerHTML = 100*prog + "%";
+
+var hrText = document.getElementById("heartrate");
+hrText.text += hrm.heartRate;
+console.log(hrText.text)
